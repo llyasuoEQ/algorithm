82c6e020644612ac5a5a5ee4db54f89d3ae6004d
梁亮亮 <liangliangliang@luojilab.com>
2021-09-22 19:44:13 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module myProject/algorithm
 
 go 1.13
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0
+)
---
+++ sort/insert_sort/insert_sort.go
@@ -0,0 +1,17 @@
+package insert_sort
+
+func Sort(arr []int64) []int64 {
+	arrLen := len(arr)
+	if arrLen < 1 {
+		return arr
+	}
+	for i := 1; i < arrLen; i++ {
+		tmp := arr[i]
+		j := i
+		for ; (j > 0) && (tmp < arr[j-1]); j-- {
+			arr[j] = arr[j-1]
+		}
+		arr[j] = tmp
+	}
+	return arr
+}

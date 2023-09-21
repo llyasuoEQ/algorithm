1038bfe5f87191d95ea2c2f7a74f3c0ed7458d79
liangliangliang <liangliangliang@luojilab.com>
2023-09-21 17:05:01 +0800

    update .gitignore

--- string_match/bm/bm.go
+++ string_match/bm/bm.go
@@ -2,9 +2,8 @@ package bm
 
 // BsSearch1... 利用算法思想直接暴力求解
 func BsSearch1(s, p string) int {
-	// si, sl := 0, len(s)
-	// pi, pl := 0, len(p)
-	// for ; si < sl; si++ {
-	// }:
+	si, sl := 0, len(s)
+	for ; si < sl; si++ {
+	}
 	return 0
 }
